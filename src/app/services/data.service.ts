030da76d97638b623add06b2842c92be31673711
Nagalakshmi-A <nagalakshmi@entrans.io>
2024-07-17 17:42:33 +0530

    some changes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { filter, map, tap} from 'rxjs/operators';
-//  this is data service application
+//  this is data service application...
 
 @Injectable({
   providedIn: 'root'
